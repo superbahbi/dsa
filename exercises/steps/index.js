d85700cac599680a2d0721f1ca49ea60cfda88cf
Bobby Lee <superbahbi@gmail.com>
2022-08-01 19:52:51 -0700

    steps

---
+++ exercises/steps/index.js
@@ -0,0 +1,34 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n) {
+  for (let i = 0; i < n; i++) {
+    let t = "";
+    for (let j = 0; j < n; j++) {
+      if (i >= j) {
+        t += "#";
+      } else {
+        t += " ";
+      }
+    }
+    console.log(t);
+  }
+}
+steps(5);
+module.exports = steps;
